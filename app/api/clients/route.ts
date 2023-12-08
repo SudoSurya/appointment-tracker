6100dc67c8379d7e5aa578c24b4da2e796d14070
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-12-08 20:14:25 +0530

    fixed some build errors

--- app/api/clients/route.ts
+++ app/api/clients/route.ts
@@ -1,7 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { promises as fs } from 'fs';
 import { NextResponse } from 'next/server';
-export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+export const GET = async (req: Request, res: Response) => {
     const fileData = await fs.readFile(process.cwd() + '/app/data.json', 'utf8');
     const data = JSON.parse(fileData);
     return NextResponse.json(data, { status: 200 });
